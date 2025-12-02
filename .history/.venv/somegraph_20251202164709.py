4f749778f3c05c2363ef527b601d3b0a4c8b8bdd
Shrrayashwinz <sshrrayash@gmail.com>
2025-12-02 16:48:29 -0500

    Initialized my labs for the graph.

---
+++ .history/.venv/somegraph_20251202164709.py
@@ -0,0 +1,15 @@
+"""
+Program Name: "simple_graph.py"
+
+Author: Shrrayash Srinivasan
+
+Purpose of this program: I will be making a (simple) graph using matplotlib.
+
+Date: December 2, 2025
+
+"""
+import matplotlib.pyplot as plt
+
+word = "started"
+
+print("I just got", word, "learning how to make graphs using matplotlib!")
